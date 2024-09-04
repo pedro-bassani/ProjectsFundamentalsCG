c5cf216246dccb44a45d99b4acac41355640b4fa
Pedro Bassani <pedrobassanihenrique@gmail.com>
2024-09-04 15:28:39 -0300

    Add files via upload

---
+++ Lista 1/List 1_Problem 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+
+    char name [15];
+
+    printf("What is your name? ");
+    scanf("%14s", name);
+    printf("Welcome, %s !!", name);
+
+    return 0;
+
+}
